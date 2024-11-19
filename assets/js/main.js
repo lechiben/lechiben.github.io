09b40a43f11643053f6c1020e7086cf80ea48f4d
Ben Le <lechiben@hotmail.com>
2024-11-18 19:04:36 -0800

    update scrool bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,32 +34,6 @@ const shadowHeader = () => {
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
-// const contactForm = document.getElementById('contact-form'),
-//     contactMessage = document.getElementById('contact-message')
-
-// const sendEmail = (e) => {
-//     e.preventDefault()
-
-//     // serviceID - templateID - #form - publicKey
-//     emailjs.sendForm('service_mcttcrl','template_j073jqn', '#contact-form','SYmCpRc5OQwZw2KE6')
-//     .then(() => {
-//         //Show sent message
-//         contactMessage.textContent = 'Message sent successfully ✅'
-
-//         // Remove message after five seconds
-//         setTimeout(() => {
-//             contactMessage.textContent = ''
-//     }, 5000)
-
-//         // Clear input fields
-//         contactForm.reset()
-// }, () => {
-//      // Show error message
-//      contactMessage.textContent = 'Message not sent (service error) ❌'
-// })    
-// }
-
-// contactForm.addEventListener('submit', sendEmail)
 
 const contactForm = document.querySelector('#contact-form');
 const contactMessage = document.querySelector('.contact__message');
@@ -92,7 +66,15 @@ contactForm.addEventListener('submit', sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // when the scroll is higher than 350 viewport heigh, add the show-scroll class to the tag with the scrollup class
+    this.scrollY >=350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
 
+}
+window.addEventListener('scroll', scrollUp)
+ 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
