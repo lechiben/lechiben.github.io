547719977e7c38fe9e8d0bbb8b7813e6eeb62be1
Ben Le <lechiben@hotmail.com>
2025-06-11 16:01:02 -0700

    Update new format

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # lechiben.github.io
 Le Chi Ben Interactive CV
+Improvised from Massively by HTML5 UP html5up.net | @ajlkn
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,162 +1,258 @@
-/*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+/*
+	Massively by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
-if(navToggle){
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
-}
+(function($) {
 
-if(navClose){
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
-}
-/*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper'),
+		$header = $('#header'),
+		$nav = $('#nav'),
+		$main = $('#main'),
+		$navPanelToggle, $navPanel, $navPanelInner;
 
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+	// Breakpoints.
+		breakpoints({
+			default:   ['1681px',   null       ],
+			xlarge:    ['1281px',   '1680px'   ],
+			large:     ['981px',    '1280px'   ],
+			medium:    ['737px',    '980px'    ],
+			small:     ['481px',    '736px'    ],
+			xsmall:    ['361px',    '480px'    ],
+			xxsmall:   [null,       '360px'    ]
+		});
 
+	/**
+	 * Applies parallax scrolling to an element's background image.
+	 * @return {jQuery} jQuery object.
+	 */
+	$.fn._parallax = function(intensity) {
 
-/*=============== SHADOW HEADER ===============*/
-const shadowHeader = () => {
-    const header = document.getElementById('header')
-    // When the scroll is greater than 5 viewport height, add the shadow-header clas to the header tag
-    this.scrollY >= 50 ? header.classList.add('shadow-header')
-                        : header.classList.remove('shadow-header')
-}
-window.addEventListener('scroll', shadowHeader)
+		var	$window = $(window),
+			$this = $(this);
 
-/*=============== EMAIL JS ===============*/
+		if (this.length == 0 || intensity === 0)
+			return $this;
 
-const contactForm = document.querySelector('#contact-form');
-const contactMessage = document.querySelector('.contact__message');
+		if (this.length > 1) {
 
-const sendEmail = (e) => {
-    e.preventDefault();
+			for (var i=0; i < this.length; i++)
+				$(this[i])._parallax(intensity);
 
-    // serviceID - templateID - #form - publicKey
-    emailjs.sendForm('service_mcttcrl', 'template_j073jqn', contactForm, 'SYmCpRc5OQwZw2KE6')
-        .then(() => {
-            // Show sent message
-            contactMessage.textContent = 'Message sent successfully ✅';
+			return $this;
 
-            // Remove message after five seconds
-            setTimeout(() => {
-                contactMessage.textContent = '';
-            }, 5000);
+		}
 
-            // Clear input fields
-            contactForm.reset();
-        })
-        .catch((error) => {
-            // Show error message
-            contactMessage.textContent = 'Message not sent (service error) ❌';
-            console.error('EmailJS error:', error);
-        });
-};
+		if (!intensity)
+			intensity = 0.25;
 
-contactForm.addEventListener('submit', sendEmail);
+		$this.each(function() {
 
-/*=============== SHOW SCROLL UP ===============*/ 
+			var $t = $(this),
+				$bg = $('<div class="bg"></div>').appendTo($t),
+				on, off;
 
-const scrollUp = () => {
-    const scrollUp = document.getElementById('scroll-up')
-    // when the scroll is higher than 350 viewport heigh, add the show-scroll class to the tag with the scrollup class
-    this.scrollY >=350 ? scrollUp.classList.add('show-scroll')
-                        : scrollUp.classList.remove('show-scroll')
+			on = function() {
 
-}
-window.addEventListener('scroll', scrollUp)
- 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+				$bg
+					.removeClass('fixed')
+					.css('transform', 'matrix(1,0,0,1,0,0)');
 
-const sections = document.querySelectorAll('section[id]')
+				$window
+					.on('scroll._parallax', function() {
 
-const scrollActive = () => {
-    const scrollDown = window.scrollY
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight,
-                sectionTop = current.offsetTop - 58,
-                sectionID = current.getAttribute('id'),
-                sectionClass = document.querySelector('.nav__menu a[href*=' + sectionID + ']')
+						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
 
-                if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-                    sectionClass.classList.add('active-link')
-                }else{
-                    sectionClass.classList.remove('active-link')
-                }
-            })
-        }
-        window.addEventListener('scroll', scrollActive)
+						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
 
-/*=============== DARK LIGHT THEME ===============*/ 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'ri-sun-line'
+					});
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+			};
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrenttheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+			off = function() {
 
-// We validate if the user previously chose a topic 
-if (selectedTheme) {
-    // If the validation is fulfilled, we ask what the isses was to know if we activated or deactivated the dark
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
-}
+				$bg
+					.addClass('fixed')
+					.css('transform', 'none');
 
-//Activate/ deactivate the theme manually with the button 
-themeButton.addEventListener('click', () => {
-    //Add or remove the dark/ icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current iscon that user chose
-    localStorage.setItem('selected-theme', getCurrenttheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-}) 
+				$window
+					.off('scroll._parallax');
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-// const sr = ScrollReveal({
-//     origin: 'top',
-//     distance: '60px',
-//     duration: 2500,
-//     delay: 400,
-//     // reset: true //Animation repeat
-// })
+			};
 
-// sr.reveal(`.home__perfil, .about__image`, {origin: 'right'})
-// sr.reveal(`.home__name, .home__info, 
-//             .about__container .section__title-1, 
-//             .about__info,
-//             .contact__social,
-//             .contact__data`, {origin: 'left'})
-// sr.reveal(`.services__card, .projects__card`, {interval: 100})
+			// Disable parallax on ..
+				if (browser.name == 'ie'			// IE
+				||	browser.name == 'edge'			// Edge
+				||	window.devicePixelRatio > 1		// Retina/HiDPI (= poor performance)
+				||	browser.mobile)					// Mobile devices
+					off();
 
-document.addEventListener('DOMContentLoaded', () => {
-    const sr = ScrollReveal({
-        origin: 'top',
-        distance: '60px',
-        duration: 2500,
-        delay: 400,
-        //reset: true //Animation repeat
-    })
+			// Enable everywhere else.
+				else {
 
-    sr.reveal(`.home__perfil, .about__image`, {origin: 'right'})
-    sr.reveal(`.home__name, .home__info, 
-                .about__container .section__title-1, 
-                .about__info,
-                .contact__social,
-                .contact__data`, {origin: 'left'})
-    sr.reveal(`.services__card, .projects__card`, {interval: 100})
-})
+					breakpoints.on('>large', on);
+					breakpoints.on('<=large', off);
+
+				}
+
+		});
+
+		$window
+			.off('load._parallax resize._parallax')
+			.on('load._parallax resize._parallax', function() {
+				$window.trigger('scroll');
+			});
+
+		return $(this);
+
+	};
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('.scrolly').scrolly();
+
+	// Background.
+		$wrapper._parallax(0.925);
+
+	// Nav Panel.
+
+		// Toggle.
+			$navPanelToggle = $(
+				'<a href="#navPanel" id="navPanelToggle">Menu</a>'
+			)
+				.appendTo($wrapper);
+
+			// Change toggle styling once we've scrolled past the header.
+				$header.scrollex({
+					bottom: '5vh',
+					enter: function() {
+						$navPanelToggle.removeClass('alt');
+					},
+					leave: function() {
+						$navPanelToggle.addClass('alt');
+					}
+				});
+
+		// Panel.
+			$navPanel = $(
+				'<div id="navPanel">' +
+					'<nav>' +
+					'</nav>' +
+					'<a href="#navPanel" class="close"></a>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right',
+					target: $body,
+					visibleClass: 'is-navPanel-visible'
+				});
+
+			// Get inner.
+				$navPanelInner = $navPanel.children('nav');
+
+			// Move nav content on breakpoint change.
+				var $navContent = $nav.children();
+
+				breakpoints.on('>medium', function() {
+
+					// NavPanel -> Nav.
+						$navContent.appendTo($nav);
+
+					// Flip icon classes.
+						$nav.find('.icons, .icon')
+							.removeClass('alt');
+
+				});
+
+				breakpoints.on('<=medium', function() {
+
+					// Nav -> NavPanel.
+						$navContent.appendTo($navPanelInner);
+
+					// Flip icon classes.
+						$navPanelInner.find('.icons, .icon')
+							.addClass('alt');
+
+				});
+
+			// Hack: Disable transitions on WP.
+				if (browser.os == 'wp'
+				&&	browser.osVersion < 10)
+					$navPanel
+						.css('transition', 'none');
+
+	// Intro.
+		var $intro = $('#intro');
+
+		if ($intro.length > 0) {
+
+			// Hack: Fix flex min-height on IE.
+				if (browser.name == 'ie') {
+					$window.on('resize.ie-intro-fix', function() {
+
+						var h = $intro.height();
+
+						if (h > $window.height())
+							$intro.css('height', 'auto');
+						else
+							$intro.css('height', h);
+
+					}).trigger('resize.ie-intro-fix');
+				}
+
+			// Hide intro on scroll (> small).
+				breakpoints.on('>small', function() {
+
+					$main.unscrollex();
+
+					$main.scrollex({
+						mode: 'bottom',
+						top: '25vh',
+						bottom: '-50vh',
+						enter: function() {
+							$intro.addClass('hidden');
+						},
+						leave: function() {
+							$intro.removeClass('hidden');
+						}
+					});
+
+				});
+
+			// Hide intro on scroll (<= small).
+				breakpoints.on('<=small', function() {
+
+					$main.unscrollex();
+
+					$main.scrollex({
+						mode: 'middle',
+						top: '15vh',
+						bottom: '-15vh',
+						enter: function() {
+							$intro.addClass('hidden');
+						},
+						leave: function() {
+							$intro.removeClass('hidden');
+						}
+					});
+
+			});
+
+		}
+
+})(jQuery);
